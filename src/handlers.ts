f300965fda2e64da17c5c36fc542dfe185272211
Alex0007 <Alex0007lolpvp@gmail.com>
2020-03-27 23:46:32 +0500

    fix code with parsing

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,33 +1,44 @@
-import { WorkerHandlers, MainItem, ChannelItem } from "@watchedcom/sdk";
-
+import { WorkerHandlers, ChannelItem } from "@watchedcom/sdk";
+import * as querystring from "qs";
 import { parseItem, parseList } from "./arte-scraper";
+
 const supportedLanguages = ["en", "fr", "de", "es", "pl", "it"];
 
 export const directoryHandler: WorkerHandlers["directory"] = async (
     input,
     ctx
 ) => {
-    const url = "https://www.arte.tv/en/videos/most-viewed/";
+    console.log("directory", input);
+    const sort = input.sort || "most-viewed";
+    const page: number = <number>input.cursor || 1;
+
+    const url =
+        `https://www.arte.tv/en/videos/${sort}/?` +
+        querystring.stringify({
+            page,
+        });
+
+    console.log({ url });
+
+    const parsedItems = await ctx
+        .fetch(url)
+        .then((resp) => resp.text())
+        .then(parseList);
 
     return {
-        items: await ctx
-            .fetch(url)
-            .then((resp) => resp.text())
-            .then(parseList)
-            .then<ChannelItem[]>((data) =>
-                data.map((_) => {
-                    return {
-                        type: "channel",
-                        ids: { id: _.href },
-                        name: _.title,
-                        images: { poster: _.thumbnailUrl },
-                    };
-                })
-            ),
-        nextCursor: null,
+        items: parsedItems.map((_) => {
+            return {
+                type: "channel",
+                ids: { id: _.id },
+                name: _.title,
+                images: { poster: _.thumbnailUrl },
+            };
+        }),
+        nextCursor: parsedItems.length ? page + 1 : null,
     };
 };
 
 export const itemHandler: WorkerHandlers["item"] = async (input, ctx) => {
+    console.log("item", input);
     throw new Error("not implemented");
 };
